efe1d46673ab7f7b44bcc1e87bbbffca459f7146
justdupp <hqxhj1990@163.com>
2018-03-11 16:08:26 +0800

    更新租户调用闪付接口

--- src/main/java/hecc/cloud/tenant/client/QuickPassClient.java
+++ src/main/java/hecc/cloud/tenant/client/QuickPassClient.java
@@ -1,9 +1,12 @@
 package hecc.cloud.tenant.client;
 
 import hecc.cloud.tenant.client.vo.CodeVO;
+import hecc.cloud.tenant.controller.BaseController.ResponseVO;
+import hecc.cloud.tenant.controller.BaseController;
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @Auther xuhoujun
@@ -12,7 +15,38 @@ import org.springframework.web.bind.annotation.RequestParam;
  */
 @FeignClient("quickpass-service")
 public interface QuickPassClient {
+
     @PostMapping("/domestic/tenant/default")
     CodeVO setDefaultTenant(@RequestParam("tenantId") Long tenantId);
+
+    @PostMapping("/domestic/code/bind")
+    CodeVO bindCode(@RequestParam("code") String code, @RequestParam("currentTenantId") Long currentTenantId);
+
+    @RequestMapping(value = "/domestic/code", method = RequestMethod.POST)
+    ResponseVO createCode(@RequestParam("tenantId") Long tenantId, @RequestParam("code") String code);
+
+    @GetMapping("/domestic/tenant/{tenantId}/code")
+    CodeVO getCodeByTenantId(@PathVariable("tenantId") Long tenantId);
+
+    @GetMapping("/domestic/tenant/{tenantId}/codes")
+    List<CodeVO> getCodeListByTenantId(@PathVariable("tenantId") Long tenantId);
+
+    @RequestMapping(value = "/domestic/code/{code}/info", method = RequestMethod.POST)
+    ResponseVO modifyCode(@PathVariable("code") String code);
+
+    @RequestMapping(value = "/domestic/code/{code}/owner", method = RequestMethod.POST)
+    ResponseVO modifyCodeOwner(@RequestParam("newOwnerId") Long newOwnerId, @PathVariable("code") String code);
+
+    @GetMapping("/domestic/tenant/{tenantId}/isNormalUser")
+    Boolean isCurrentTenantUseDefaultCode(@PathVariable("tenantId") Long tenantId);
+
+    @PostMapping("/domestic/code/default")
+    CodeVO setDefaultCode(@RequestParam("platform") String platform);
+
+    @PostMapping("/domestic/code/top")
+    void setTopCode(@RequestParam("tenantId") Long tenantId);
+
+    @GetMapping("/domestic/code/default")
+    List<CodeVO> fetchDefaultCodes();
 
 }
