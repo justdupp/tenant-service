f7b750b172de1f8bc4611c2ffb50f28145667581
justdupp <hqxhj1990@163.com>
2018-04-02 21:57:50 +0800

    更新配置文件

--- src/main/java/hecc/cloud/tenant/TenantServiceApplication.java
+++ src/main/java/hecc/cloud/tenant/TenantServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
@@ -13,6 +14,7 @@ import org.springframework.context.annotation.Bean;
 @EnableFeignClients
 @EnableAutoConfiguration(exclude = {MongoAutoConfiguration.class})
 @EnableCaching
+@EnableEurekaClient
 public class TenantServiceApplication {
 
     public static void main(String[] args) {
